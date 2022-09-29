ffc60d769ceca0006816878fcd1cb6f97fb995a3
18nguyenl <18nguyenl@gmail.com>
2022-09-29 14:33:46 -0500

    Remove omnidirection

--- src/animation.ts
+++ src/animation.ts
@@ -1,8 +1,6 @@
 import { toRadians, vec2 } from "./Math";
 import { animate } from "motion";
 import BillboardState from "./State";
-
-const PRECISION = 2;
 export default class Animation {
   directionVector: vec2;
   magnitude: vec2;
@@ -24,65 +22,12 @@ export default class Animation {
   }
 
   animate(animationRoot: HTMLElement) {
-    const rows = animationRoot.querySelectorAll(".row");
-
-    const widthStart = parseInt(
-      (rows[0] as HTMLElement).style.left.slice(0, -2)
-    );
-    const heightStart = parseInt(
-      (rows[0] as HTMLElement).style.top.slice(0, -2)
-    );
-    let upperDirection = 1;
-    let lowerDirection = 1;
-
-    if (
-      Math.sign(this.directionVector[0]) > 0 &&
-      Math.sign(this.directionVector[1]) < 0
-    ) {
-      lowerDirection = -1;
-      upperDirection = 1;
-    }
-
-    if (
-      Math.sign(this.directionVector[0]) < 0 &&
-      Math.sign(this.directionVector[1]) > 0
-    ) {
-      lowerDirection = 1;
-      upperDirection = 1;
-    }
-
-    if (
-      Math.sign(this.directionVector[0]) > 0 &&
-      Math.sign(this.directionVector[1]) > 0
-    ) {
-      upperDirection = -1;
-      lowerDirection = 1;
-    }
-
-    (rows[0] as HTMLElement).style.left = `${
-      widthStart +
-      upperDirection *
-        Math.sign(this.directionVector[0]) *
-        parseFloat(this.directionVector[0].toFixed(PRECISION)) *
-        this.magnitude[0]
-    }px`;
-    (rows[2] as HTMLElement).style.left = `${
-      widthStart +
-      lowerDirection *
-        Math.sign(this.directionVector[0]) *
-        parseFloat(this.directionVector[0].toFixed(PRECISION)) *
-        this.magnitude[0]
-    }px`;
-
     animate(
       animationRoot.querySelectorAll(".container-wrapper > .row > div"), // TODO: Generalize Equation
       {
         transform: `translate(${
-          parseFloat(this.directionVector[0].toFixed(PRECISION)) *
-          this.magnitude[0]
-        }px, ${
-          parseFloat(this.directionVector[1].toFixed(0)) * this.magnitude[1]
-        }px)`,
+          Math.round(this.directionVector[0]) * this.magnitude[0]
+        }px, ${Math.round(this.directionVector[1]) * this.magnitude[1]}px)`,
       },
       { duration: this.speed, easing: "linear", repeat: Infinity }
     );
