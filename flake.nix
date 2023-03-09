671eb54c80076829f948a94e19fc6afa812d2642
Long Nguyen <me@long-nguyen.dev>
2023-03-09 17:07:04 -0600

    Nixify project

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+    description = "This is a template to make amazing projects or other templates.";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+  
+    outputs = { self, nixpkgs, flake-utils }: 
+        flake-utils.lib.eachDefaultSystem (system:
+        let 
+            pkgs = import nixpkgs { inherit system; };
+        in rec {
+            devShells.default = pkgs.mkShell {
+                packages = [ pkgs.nodejs ];
+
+                shellHook = ''
+                    echo
+                    echo "     [[[[[  WELCOME TO THE NEBULA  ]]]]]"
+                    echo "     ᚤᛟᚢ ᚨᚱᛖ ᛒᛖᚲᛟᛗᛁᚾᚷ ᛟᚾᛖ ᚹᛁᛏᚺ ᛏᚺᛖ ᛋᛏᚨᚱᛋ"
+                    echo "     [[[[[  •+-#—*#•-##++-*+-+••## ]]]]]"
+                    echo
+                '';
+            };
+        }
+    );
+}
