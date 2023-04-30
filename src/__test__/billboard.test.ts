ba2e5a2a4eab0242d102fd661304dce6244096f8
Long Nguyen <me@long-nguyen.dev>
2023-04-30 18:33:34 -0500

    Tests cover most of the code, Finish refactor

--- src/__test__/billboard.test.ts
+++ src/__test__/billboard.test.ts
@@ -26,17 +26,31 @@ describe("billboard", () => {
         expect(document.contains(BillboardManager.styleElement)).toBeFalsy();
     });
 
+    // I essentiallly observe and compare transformations and states before then after
+    // Other tests will validate if it properly tears down
     it("should return website to original state when deinitializing billboard", async () => {
         Basic.loadContent();
 
+        const before = Basic.ticker.children;
+        const beforeClass = Array.from(Basic.ticker.classList);
         const billboard = new Billboard(Basic.ticker);
 
+        billboard.init();
+
+        expect(beforeClass).not.toStrictEqual(
+            Array.from(Basic.ticker.classList)
+        );
+
         billboard.deinit();
-        expect(billboard.system._ticker.isRendered).toBeFalsy();
+
+        const after = Basic.ticker.children;
+        const afterClass = Array.from(Basic.ticker.classList);
+
+        expect(beforeClass).toStrictEqual(afterClass);
+        expect(before).toEqual(after);
         expect(
             document.head.contains(BillboardManager.styleElement)
         ).toBeFalsy();
         expect(BillboardManager.hasBillboards).toBeFalsy();
-        expect(billboard.system._ticker.isRendered).toBeFalsy();
     });
 });
