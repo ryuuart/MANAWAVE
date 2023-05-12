9182358b49637ceaf3e594f9114467f4253a9b1e
Long Nguyen <me@long-nguyen.dev>
2023-05-11 20:23:05 -0500

    Migrate to hooks for custom playback control

--- src/anim/__test__/animation.test.ts
+++ src/anim/__test__/animation.test.ts
@@ -55,6 +55,46 @@ describe("animation system", () => {
     });
 
     describe("player", () => {
+        it("should call the playback hooks", async () => {
+            class TestPlaybackObject extends PlaybackObject {
+                testStatus: string = "";
+
+                onStart() {
+                    this.testStatus = "CUSTOM START LOGIC";
+                }
+
+                onPause(): void {
+                    this.testStatus = "CUSTOM PAUSE LOGIC";
+                }
+
+                onPlay(): void {
+                    this.testStatus = "CUSTOM PLAY LOGIC";
+                }
+
+                onStop(): void {
+                    this.testStatus = "CUSTOM STOP LOGIC";
+                }
+            }
+
+            const player = new TestPlaybackObject();
+
+            player.start();
+
+            expect(player.testStatus).toEqual("CUSTOM START LOGIC");
+
+            player.pause();
+
+            expect(player.testStatus).toEqual("CUSTOM PAUSE LOGIC");
+
+            player.play();
+
+            expect(player.testStatus).toEqual("CUSTOM PLAY LOGIC");
+
+            player.stop();
+
+            expect(player.testStatus).toEqual("CUSTOM STOP LOGIC");
+        });
+
         it("should have proper control state throughout animation", async () => {
             class TestPlaybackObject extends PlaybackObject {}
 
