7581d8ac3b0541141fe99de0e7e8f5022b3619a3
Long Nguyen <me@long-nguyen.dev>
2023-05-12 13:34:13 -0500

    Add tests for System

---
+++ src/anim/__test__/unit/animation.test.ts
@@ -0,0 +1,52 @@
+import { System } from "@billboard/lib";
+
+describe("animation controller", () => {});
+
+class BlankSystem extends System {
+    dt: DOMHighResTimeStamp;
+    t: DOMHighResTimeStamp;
+    stringCanvas: string;
+
+    constructor() {
+        super();
+
+        this.dt = 0;
+        this.t = 0;
+
+        this.stringCanvas = "not rendered";
+    }
+
+    onUpdate(dt: number, t: number): void {
+        this.dt = dt;
+        this.t = t;
+    }
+
+    onDraw(): void {
+        this.stringCanvas = "rendered";
+    }
+}
+
+describe("system", () => {
+    it("should invoke onUpdate on update", async () => {
+        const system = new BlankSystem();
+        system.start();
+
+        system.update(10, 10);
+
+        expect(system.t).toEqual(10);
+        expect(system.dt).toEqual(10);
+
+        system.stop();
+    });
+
+    it("should invoke onDraw on draw", async () => {
+        const system = new BlankSystem();
+        system.start();
+
+        system.draw();
+
+        expect(system.stringCanvas).toEqual("rendered");
+
+        system.stop();
+    });
+});
