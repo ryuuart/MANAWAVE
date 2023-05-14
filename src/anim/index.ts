de495d532eb5980f37e6b7e0cb7c121f8c6f5201
Long Nguyen <me@long-nguyen.dev>
2023-05-13 19:05:52 -0500

    Revert "Add base for DrawObjects"
    This reverts commit a19e22b4d1444933aa98ecb2910f1bb7c4649151.

--- src/anim/index.ts
+++
@@ -1,5 +0,0 @@
-import AnimationController from "@billboard/anim/AnimationController";
-import System from "./System";
-import DrawObject from "./DrawObject";
-
-export { System, DrawObject, AnimationController };
