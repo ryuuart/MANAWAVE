878488dfc1845c9554692d4589e1ee0f53f515cc
Long Nguyen <me@long-nguyen.dev>
2023-05-15 19:50:19 -0500

    No JS initialization Fixes #60

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,22 @@ if (!customElements.get("billboard-ticker")) {
     customElements.define("billboard-ticker", Component);
 }
 
+// for a no-js approach while still using divs
+window.addEventListener(
+    "load",
+    () => {
+        const divBillboardTickers =
+            document.querySelectorAll(`[billboard-ticker]`);
+
+        for (const ticker of divBillboardTickers) {
+            new Billboard(ticker as HTMLElement);
+        }
+    },
+    {
+        once: true,
+    }
+);
+
 AnimationController.start();
 
 export { Billboard };
