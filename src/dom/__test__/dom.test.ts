cd08158b92f58d28d6fe388b337442a438355c94
Long Nguyen <me@long-nguyen.dev>
2023-05-31 18:29:29 -0500

    Add repetition measurement, Clarify previous DOM tests,

--- src/dom/__test__/dom.test.ts
+++ src/dom/__test__/dom.test.ts
@@ -1,5 +1,5 @@
 import Square from "test/pages/square/Square";
-import { measure } from "../measure";
+import { getRepetitions, measure } from "../measure";
 
 describe("dom", () => {
     beforeEach(() => {
@@ -14,6 +14,7 @@ describe("dom", () => {
         testElement.style.width = "120px";
         testElement.style.height = "130px";
 
+        // our change in measurement should be observed
         const rect = measure(testElement);
 
         const observedElement = await $(`#${testElement.id}`);
@@ -25,10 +26,34 @@ describe("dom", () => {
         expect(rect?.width).toEqual(observedRect.width);
         expect(rect?.height).toEqual(observedRect.height);
 
+        // if the element is removed or disconnected from the page,
+        // there is no returned measurement
         testElement.remove();
 
         let disconnectedRect = measure(testElement);
 
         expect(disconnectedRect).toBe(null);
+    });
+
+    it("should measure the correct number of repetitions in different contexts", async () => {
+        const uniformRectLarge = { width: 1000, height: 1000 };
+        const uniformRectSmall = { width: 100, height: 100 };
+        const nonUniformRectSmall = { width: 123, height: 123 };
+
+        // base uniform case where things fit in perfectly
+        const case1 = getRepetitions(uniformRectLarge, uniformRectSmall);
+        expect(case1).toEqual({ horizontal: 10, vertical: 10 });
+
+        // a repeatable larger than its container should "repeat" 1 time
+        const case2 = getRepetitions(uniformRectSmall, uniformRectLarge);
+        expect(case2).toEqual({ horizontal: 1, vertical: 1 });
+
+        // if they're the same length, it should "repeat" once
+        const case3 = getRepetitions(uniformRectSmall, uniformRectSmall);
+        expect(case3).toEqual({ horizontal: 1, vertical: 1 });
+
+        // an uneven, smaller repeatable should round up to fill the space
+        const case4 = getRepetitions(uniformRectLarge, nonUniformRectSmall);
+        expect(case4).toEqual({ horizontal: 9, vertical: 9 });
     });
 });
--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,13 @@ interface Rect {
     width: number;
     height: number;
 }
+
+interface Positionable {
+    x: number;
+    y: number;
+}
+
+interface RepetitionCount {
+    horizontal: number;
+    vertical: number;
+}
