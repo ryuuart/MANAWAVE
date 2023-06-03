7f13c566749f3973941584c028a1b6e4c667acef
Long Nguyen <me@long-nguyen.dev>
2023-06-02 21:18:26 -0500

    Refine onUpdate simulation refactor

---
+++ src/ticker/simulation.ts
@@ -0,0 +1,100 @@
+import { getRepetitions } from "@ouroboros/dom/measure";
+import { TickerStateData } from "./state";
+import { accumulateVec2, normalize, toRadians } from "./math";
+import { Item, extractPosition } from "./item";
+
+type TSimulationContext = {
+    ticker: { size: Rect };
+    item: { size: Rect };
+    limits: DirectionalCount;
+    intendedDirection: vec2;
+    actualDirection: vec2;
+    speed: number;
+};
+
+function getTLimits(ticker: Rect, item: Rect): DirectionalCount {
+    const limits = getRepetitions(ticker, item);
+    limits.horizontal *= ticker.width;
+    limits.vertical *= item.height;
+
+    return limits;
+}
+
+function makeDirectionAccumulator() {
+    const accumulate = accumulateVec2({ x: 0, y: 0 });
+
+    return function (prevPos: vec2, currPos: vec2): vec2 {
+        const delta = { x: currPos.x - prevPos.x, y: currPos.y - prevPos.y };
+        return accumulate(delta);
+    };
+}
+
+function moveItem(
+    item: Item,
+    { intendedDirection, speed }: TSimulationContext
+) {
+    // directions should not have any magnitude and should be normalized
+    const normalizedDirection = normalize(intendedDirection);
+
+    item.x += 1 * speed * normalizedDirection.x;
+    item.y += 1 * speed * normalizedDirection.y;
+}
+
+function loopItem(item: Item, context: TSimulationContext) {
+    const itemSize = context.item.size;
+
+    const { actualDirection, limits } = context;
+    if (actualDirection.x > 0 && item.x >= limits.horizontal) {
+        item.x = -itemSize.width;
+    } else if (actualDirection.x < 0 && item.x <= -itemSize.width) {
+        item.x = limits.horizontal;
+    }
+    if (actualDirection.y > 0 && item.y >= limits.vertical) {
+        item.y = -itemSize.height;
+    } else if (actualDirection.y < 0 && item.y <= -itemSize.height) {
+        item.y = limits.vertical;
+    }
+}
+
+export function simulateItem(
+    item: Item,
+    state: TickerStateData,
+    dt: DOMHighResTimeStamp,
+    t: DOMHighResTimeStamp
+) {
+    const sTicker = state.ticker;
+    const sItem = state.item;
+
+    const tContext: TSimulationContext = {
+        ticker: sTicker,
+        item: sItem,
+        limits: getTLimits(sTicker.size, sItem.size),
+        intendedDirection: {
+            x: Math.cos(toRadians(state.direction)),
+            y: Math.sin(toRadians(state.direction)),
+        },
+        speed: state.speed,
+        actualDirection: { x: 0, y: 0 },
+    };
+
+    const accumulateDirection = makeDirectionAccumulator();
+    const initialPosition = extractPosition(item);
+
+    // TODO: override hook here
+
+    tContext.actualDirection = accumulateDirection(
+        initialPosition,
+        extractPosition(item)
+    );
+
+    moveItem(item, tContext);
+
+    tContext.actualDirection = accumulateDirection(
+        tContext.actualDirection,
+        extractPosition(item)
+    );
+
+    loopItem(item, tContext);
+
+    item.lifetime += dt;
+}
--- src/ticker/system.ts
+++ src/ticker/system.ts
@@ -1,17 +1,10 @@
 import { System } from "@ouroboros/anim";
 import { Container, clearContainer } from "./container";
 import { Item } from "./item";
-import TickerState from "./state";
+import { TickerState } from "./state";
 import { getRepetitions } from "@ouroboros/dom/measure";
 import { fillGrid, layoutGrid } from "./layout";
-import { accumulateVec2, normalize, toRadians } from "./math";
-
-type ItemState = {
-    direction: vec2;
-    position: vec2;
-    speed: number;
-    time: { dt: DOMHighResTimeStamp; t: DOMHighResTimeStamp };
-};
+import { simulateItem } from "./simulation";
 
 export default class TickerSystem extends System {
     state: TickerState;
@@ -31,16 +24,17 @@ export default class TickerSystem extends System {
     }
 
     onStart() {
+        const state = this.state.current;
         const gridOptions = {
-            grid: this.state.current.ticker,
-            item: this.state.current.item,
+            grid: state.ticker,
+            item: state.item,
             repetitions: getTickerRepetitions(
-                this.state.current.ticker.size,
-                this.state.current.item.size
+                state.ticker.size,
+                state.item.size
             ),
             offset: {
-                x: -this.state.current.item.size.width,
-                y: -this.state.current.item.size.height,
+                x: -state.item.size.width,
+                y: -state.item.size.height,
             },
         };
 
@@ -55,96 +49,11 @@ export default class TickerSystem extends System {
     onUpdate(dt: DOMHighResTimeStamp, t: DOMHighResTimeStamp) {
         // iterate through all items
         for (const item of this.container.contents) {
-            item.lifetime += dt;
-
-            // precalculate and store modified state inside another state object
-            let itemState = getItemState(item, this.state, { dt, t });
-
-            const accumulateDirection = makeDirectionAccumulator();
-            const initialPosition = structuredClone(itemState.position);
-
-            // TODO: add an override hook
-            // if (this._onItemUpdated) {
-            //     Object.assign(itemState, this._onItemUpdated(itemState));
-            // }
-
-            // there's a direction we want, and one that is actually happening
-            // useful when there's custom logic that overrides the theoretical / intended direction
-            const initialDirection = accumulateDirection(
-                initialPosition,
-                itemState.position
-            );
-
-            // perform modification
-            solveItemMotion(item, itemState);
-
-            const finalDirection = accumulateDirection(initialDirection, {
-                x: item.x,
-                y: item.y,
-            });
-
-            // test and set where items should loop once out of bounds
-            loopItem(item, finalDirection, this.state);
+            simulateItem(item, this.state.current, dt, t);
         }
     }
 
     onDraw() {}
-}
-
-function getTickerLimits(state: TickerState): DirectionalCount {
-    const tickerSize = state.current.ticker.size;
-    const itemSize = state.current.item.size;
-
-    const limits = getRepetitions(tickerSize, itemSize);
-    limits.horizontal *= itemSize.width;
-    limits.vertical *= itemSize.height;
-
-    return limits;
-}
-
-function getItemState(
-    item: Item,
-    state: TickerState,
-    time: { t: DOMHighResTimeStamp; dt: DOMHighResTimeStamp }
-): ItemState {
-    return {
-        direction: {
-            x: Math.cos(toRadians(state.current.direction)),
-            y: Math.sin(toRadians(state.current.direction)),
-        },
-        position: {
-            x: item.x,
-            y: item.y,
-        },
-        speed: state.current.speed,
-        time,
-    };
-}
-
-function loopItem(item: Item, finalDirection: vec2, state: TickerState) {
-    const tickerSize = state.current.ticker.size;
-    const itemSize = state.current.item.size;
-    const limits = getTickerLimits(state);
-
-    if (finalDirection.x > 0 && item.x >= limits.horizontal) {
-        item.x = -itemSize.width;
-    } else if (finalDirection.x < 0 && item.x <= -itemSize.width) {
-        item.x = limits.horizontal;
-    }
-    if (finalDirection.y > 0 && item.y >= limits.vertical) {
-        item.y = -itemSize.height;
-    } else if (finalDirection.y < 0 && item.y <= -itemSize.height) {
-        item.y = limits.vertical;
-    }
-}
-
-function makeDirectionAccumulator() {
-    const accumulate = accumulateVec2({ x: 0, y: 0 });
-
-    return function (prevPos: vec2, currPos: vec2): vec2 {
-        const delta = { x: currPos.x - prevPos.x, y: currPos.y - prevPos.y };
-        return accumulate(delta);
-    };
 }
 
 function getTickerRepetitions(
@@ -156,12 +65,4 @@ function getTickerRepetitions(
     repetitions.vertical += 2;
 
     return repetitions;
-}
-
-function solveItemMotion(item: Item, state: ItemState) {
-    // directions should not have any magnitude and should be normalized
-    const normalizedDirection = normalize(state.direction);
-
-    item.x = state.position.x + 1 * state.speed * normalizedDirection.x;
-    item.y = state.position.y + 1 * state.speed * normalizedDirection.y;
 }
