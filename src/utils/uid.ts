2bac7a0f793536fa84fa1b8cda8bfa70305ec3bb
Long Nguyen <me@long-nguyen.dev>
2023-06-06 21:43:02 -0500

    Fix UID imports

--- src/utils/uid.ts
+++ src/utils/uid.ts
@@ -2,9 +2,9 @@
  * Generates a lightweight unique ID with a light risk of collision
  * @returns a lightweight unique ID
  */
-const uid = function () {
+export function uid() {
     return String(Date.now().toString(32) + Math.random().toString(16)).replace(
         /\./g,
         ""
     );
-};
+}
