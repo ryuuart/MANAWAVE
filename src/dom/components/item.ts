f132d11fe702560f01277cc6a98e1788e737de48
Long Nguyen <me@long-nguyen.dev>
2023-06-17 00:22:34 -0400

    Add template cloning into item option

--- src/dom/components/item.ts
+++ src/dom/components/item.ts
@@ -1,12 +1,14 @@
 import { Item } from "@ouroboros/ticker/item";
 import { Component } from "../component";
 import styles from "../styles/item.module.css";
+import TemplateComponent from "./template";
 
 export default class ItemComponent extends Component {
     position: vec2;
-    constructor(id: string, item: Item) {
+    constructor(id: string, item: Item, template?: TemplateComponent) {
         const element = document.createElement("div");
         element.classList.add(styles.item);
+        if (template) element.append(...template.cloneDOM());
 
         super(element, id);
 
