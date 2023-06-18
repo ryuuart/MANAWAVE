87387fa98d2566a9d0e3360d9e18ba93220280aa
Long Nguyen <me@long-nguyen.dev>
2023-06-17 22:15:18 -0400

    Add documentation

--- src/dom/scene.ts
+++ src/dom/scene.ts
@@ -1,5 +1,8 @@
 import { Component } from "./component";
 
+/**
+ * Contains tree of components
+ */
 export default class Scene {
     private _components: Map<string, Component>;
 
@@ -11,20 +14,39 @@ export default class Scene {
         return this._components.values();
     }
 
+    /**
+     * Retrieves a component given an ID
+     * @param id the id of the component
+     * @returns the component if there is one
+     */
     get(id: string) {
         return this._components.get(id);
     }
 
+    /**
+     * Return whether or not a specified {@link Component} exists in the {@link Scene} or not
+     *
+     * @param selection a component ID or {@link Component} to search for
+     * @returns true if a component exists in the scene
+     */
     has(selection: string | Component) {
         if (selection instanceof Component)
             return this._components.has(selection.id);
         else return this._components.has(selection);
     }
 
+    /**
+     * Add or set a component in the scene.
+     * @param component a component to update or add in the scene
+     */
     set(component: Component) {
         this._components.set(component.id, component);
     }
 
+    /**
+     * Removes the selected {@link Component} from the scene if it exists.
+     * @param selection a component ID or {@link Component}
+     */
     remove(selection: string | Component) {
         if (selection instanceof Component)
             this._components.delete(selection.id);
