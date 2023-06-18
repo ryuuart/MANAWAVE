de4174716d00ca0420495a9259581530a4129b88
Long Nguyen <me@long-nguyen.dev>
2023-06-18 00:04:53 -0400

    Add styling for custom element

--- src/dom/element.ts
+++ src/dom/element.ts
@@ -17,6 +17,7 @@ export default class WebComponent extends HTMLElement {
     }
 
     disconnectedCallback() {
+        this.ouroboros?.stop();
         this.ouroboros = null;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AnimationController } from "./anim";
 import { Ouroboros } from "./ouroboros";
 import WebComponent from "./dom/element";
+import "./dom/styles/styles.css";
 
 if (!customElements.get("ouroboros-ticker")) {
     customElements.define("ouroboros-ticker", WebComponent);
