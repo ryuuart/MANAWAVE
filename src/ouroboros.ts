99a4ee1105676ad064e340ed721c249f0e733373
Long Nguyen <me@long-nguyen.dev>
2023-06-24 15:02:48 -0500

    Update measurement code, Remove simulation code,

--- src/ouroboros.ts
+++ src/ouroboros.ts
@@ -1,12 +1,13 @@
 import PlaybackObject from "./anim/PlaybackObject";
 import { convertDirection, mergeOOptions } from "./dom/attributes";
-import { measure, measureElementBox } from "./dom/measure";
+import { Dimensions, MeasurementBox } from "./dom/measure";
 import TickerSystem from "./ticker/system";
 import styles from "./dom/styles/ouroboros.module.css";
 import { AnimationController } from "./anim";
 
 export class Ouroboros extends PlaybackObject {
     private simulation!: TickerSystem;
+    private dimensions: Dimensions;
 
     private _selector: Parameters<Document["querySelector"]>[0] | HTMLElement;
     private _options?: Partial<Ouroboros.Options>;
@@ -19,6 +20,7 @@ export class Ouroboros extends PlaybackObject {
 
         this._selector = selector;
         this._options = options;
+        this.dimensions = new Dimensions();
 
         this.start();
     }
@@ -29,29 +31,31 @@ export class Ouroboros extends PlaybackObject {
         else element = document.querySelector(this._selector) as HTMLElement;
 
         if (element) {
+            this.dimensions.setEntry("root", element, (rect) => {
+                console.log(rect);
+                // this.simulation.updateSize({ ticker: rect });
+            });
+
+            const mBox = new MeasurementBox(...element.children);
+            mBox.startMeasuringFrom(element);
+
+            this.dimensions.setEntry("item", mBox, (rect) => {
+                console.log(rect);
+                // this.simulation.updateSize({ item: rect });
+            });
+
+            const tSizes = {
+                ticker: this.dimensions.get("root")!,
+                item: this.dimensions.get("item")!,
+            };
+
             element.classList.add(styles.ouroboros);
             const currOptions = mergeOOptions(element, this._options);
 
-            // TODO: in the future, there will be logic that guarantees
-            // measurement without worrying about order
             const tProps = {
                 speed: currOptions.speed,
                 direction: convertDirection(currOptions.direction),
             };
-
-            const tSizes = {
-                ticker: measure(element)!,
-                item: measureElementBox(element.children),
-            };
-
-            this.simulation = new TickerSystem(element, {
-                attributes: tProps,
-                sizes: tSizes,
-            });
-
-            AnimationController.registerSystem(this.simulation);
-            this.simulation.start();
-            if (!currOptions.autoplay) this.simulation.pause();
         } else {
             throw new Error("Element not found for Ouroboros.");
         }
