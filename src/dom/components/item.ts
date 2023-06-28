a267327b43ae0987b718e1457141b5709ad8af18
Long Nguyen <me@long-nguyen.dev>
2023-06-27 20:47:02 -0500

    Add position reference for consistency

--- src/dom/components/item.ts
+++ src/dom/components/item.ts
@@ -2,6 +2,8 @@ import { Item } from "@ouroboros/ticker/item";
 import { Component } from "../component";
 import styles from "../styles/item.module.css";
 export default class ItemComponent extends Component {
+    private _position: vec2;
+
     constructor(item: Item, template?: DocumentFragment) {
         const element = document.createElement("div");
         element.classList.add(styles.item);
@@ -9,7 +11,15 @@ export default class ItemComponent extends Component {
 
         super(element, item.id);
 
+        this._position = item.position;
         this.setPosition(structuredClone(item.position));
+    }
+
+    /**
+     * Get the current position of the component
+     */
+    get position(): vec2 {
+        return structuredClone(this._position);
     }
 
     /**
@@ -18,6 +28,7 @@ export default class ItemComponent extends Component {
      * @param pos new position to set
      */
     setPosition(pos: vec2) {
+        this._position = structuredClone(pos);
         this.html.style.transform = `translate(${pos.x}px, ${pos.y}px)`;
     }
 }
