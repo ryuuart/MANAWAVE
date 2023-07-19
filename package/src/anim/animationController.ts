5b6d285d2a23d3d438083f23cad5aa9c69b7f4d8
Long Nguyen <me@long-nguyen.dev>
2023-07-19 03:41:14 -0500

    Fix case

---
+++ package/src/anim/animationController.ts
@@ -0,0 +1,91 @@
+import { System } from ".";
+import PlaybackObject from "./playbackObject";
+
+/**
+ * Singleton that controls all animations
+ */
+class AnimationController extends PlaybackObject {
+    private _systems: Set<System> = new Set();
+
+    private _renderID: number | undefined;
+
+    private _currentTime: DOMHighResTimeStamp = window.performance.now();
+    private _totalTime: DOMHighResTimeStamp = 0;
+    private _targetDT: DOMHighResTimeStamp = 1000 / 60.0; // Target Delta Time
+
+    /**
+     * Hook that runs when the animation controller is started.
+     * It resets time to 0 and starts the render loop.
+     */
+    onStart(): void {
+        this._totalTime = 0;
+        this._currentTime = window.performance.now();
+
+        this._renderID = window.requestAnimationFrame(this.render.bind(this));
+    }
+
+    /**
+     * Hook that clears the render loop
+     */
+    onStop() {
+        if (this._renderID) {
+            window.cancelAnimationFrame(this._renderID);
+        }
+    }
+
+    /**
+     *  Adds a {@link System} to be animated to the render loop
+     * @param system a {@link System} that contains logic that renders an animation and its logical system
+     */
+    registerSystem(system: System) {
+        this._systems.add(system);
+    }
+
+    /**
+     * Removes a {@link System} from what is currently being rendered
+     * @param system a {@link System} that contains logic that renders an animation and its logical system
+     */
+    deregisterSystem(system: System) {
+        this._systems.delete(system);
+    }
+
+    /**
+     * The render loop that runs all animation updates and draw calls.
+     * It performs semi-lockstep frame syncing.
+     *
+     * @param timestamp current time from the clock
+     */
+    render(timestamp: DOMHighResTimeStamp) {
+        let newTime = timestamp;
+        let frameTime = newTime - this._currentTime;
+        this._currentTime = newTime;
+
+        if (this.status.started && !this.status.paused) {
+            // if time has progressed
+            while (frameTime > 0.0) {
+                // if you're lagging, use the frame time
+                // if you're running super fast, sync to the fixed time
+                let dt = Math.min(frameTime, this._targetDT);
+
+                // Update the overall system
+                for (const system of this._systems) {
+                    system.update(dt, this._totalTime);
+                }
+
+                // step once
+                frameTime -= dt;
+
+                this._totalTime += dt;
+            }
+
+            // Render
+            for (const system of this._systems) {
+                system.draw();
+            }
+        }
+
+        window.requestAnimationFrame(this.render.bind(this));
+    }
+}
+
+export default new AnimationController();
---
+++ package/src/anim/system.ts
@@ -0,0 +1,49 @@
+import AnimationController from "./animationController";
+import PlaybackObject from "./playbackObject";
+
+/**
+ * An animated system that performs logical updates on its update call and
+ * visual updates on its render call. Intended to be used with {@link requestAnimationFrame }.
+ */
+export default abstract class System extends PlaybackObject {
+    /**
+     * Runs {@link System.onDraw } accounting for playback controls.
+     *
+     * @see {@link AnimationController.render }
+     */
+    draw() {
+        if (this.status.started && !this.status.paused) {
+            this.onDraw();
+        }
+    }
+
+    /**
+     * Runs {@link System.onUpdate } accounting for playback controls.
+     *
+     * @see {@link AnimationController.render }
+     *
+     * @param dt the elapsed time between frames in milliseconds (ms)
+     * @param t the total time of the running animation in milliseconds (ms)
+     */
+    update(dt: DOMHighResTimeStamp, t: DOMHighResTimeStamp) {
+        if (this.status.started && !this.status.paused) {
+            this.onUpdate(dt, t);
+        }
+    }
+
+    /**
+     * Hook running per update call in the render loop
+     * This should not perform any draw operations that affect what is displayed on the screen.
+     * This should only perform updates to logic or data.
+     *
+     * @param dt The elapsed time between frames in milliseconds (ms)
+     * @param t The total time of the running animation in milliseconds (ms)
+     */
+    abstract onUpdate(dt: DOMHighResTimeStamp, t: DOMHighResTimeStamp): void;
+
+    /**
+     * Hook running per draw call in the render loop.
+     * This should only perform operations that affect what is displayed on the screen.
+     */
+    abstract onDraw(): void;
+}
