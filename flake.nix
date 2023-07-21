0e1d1ffcb560c5843a336e747dd3a07b49a533a2
Long Nguyen <me@long-nguyen.dev>
2023-07-21 14:11:55 -0500

    Configure github actions

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
             devShells.default = pkgs.mkShell {
                 packages = [ 
                     pkgs.nodejs
+                    pkgs.act
                  ];
 
                 shellHook = ''
