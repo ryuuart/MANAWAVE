8f8befbf6ee94ef8097e7ddd1634b89e4e70ffcb
Long Nguyen <me@long-nguyen.dev>
2023-09-07 00:53:27 -0500

    Add Sandpack CORS disclaimer

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -11,6 +11,9 @@ export default defineConfig({
     head: [{
       tag: 'script',
       content: `window.addEventListener('load', () => document.querySelector('.site-title').href += 'docs/quickstart')`
+    }, {
+      tag: 'script',
+      content: "console.info('If you see sandpack CORS error, it seems to be a running issue on their end.');"
     }],
     sidebar: [
       { label: "Home", link: "/" },
